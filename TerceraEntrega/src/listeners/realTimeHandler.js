fa1ad9e7504a839ae8585a99dee9bf0d9fe9fc66
Santiago Roca <santiroca88@gmail.com>
2023-08-30 14:00:25 -0300

    Delete TerceraEntrega directory

--- TerceraEntrega/src/listeners/realTimeHandler.js
+++
@@ -1,9 +0,0 @@
-import { productService } from "../services/repositories.js";
-
-const realTimeProducts = async (io, socket) => {
-  req.logger.http("Nuevo socket conectado");
-  const products = await new productService.getAllProducts();
-  socket.emit("products", products);
-};
-
-export default realTimeProducts;
--- TerceraEntrega/src/middlewares/error.js
+++
@@ -1,5 +0,0 @@
-//ERROR HANDLER
-export default (error, req, res, next) => { // Es nuestro salvador! Éste es el que define que NUNCA caiga el server
-    req.logger.error(error);
-    res.status(error.status).send({ status: "error", error: error.message })
-}
