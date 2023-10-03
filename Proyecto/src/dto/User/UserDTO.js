2850f42f9930024ba08124894673019939afe2ce
Santiago Roca <santiroca88@gmail.com>
2023-10-02 23:22:34 -0300

    Delete Proyecto directory

--- Proyecto/src/dto/User/UserDTO.js
+++
@@ -1,9 +0,0 @@
-export default class UserDTO {
-    static getFrom = (user) => {
-        return {
-            name: user.name,
-            email: user.email,
-            role: user.role
-        }
-    }
-}
